a442474f243317972dbe7892a92c74fd45b85acc
Stefan Marx <stefan@marx-merten.de>
2024-01-24 18:53:52 +0100

    moved build command

--- buildAll.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p firmware
-
-west build -s app -b nice_nano_v2 -d build/cf-lily/left -- -DZMK_CONFIG=$(realpath ./../zmk-lily59-main/config) -DSHIELD="lily58_left nice_view_adapter nice_view" &&
-    cp build/cf-lily/left/zephyr/zmk.uf2 firmware/cf_lily_left_display.uf2
-
-west build -s app -b nice_nano_v2 -d build/cf-lily/right -- -DZMK_CONFIG=$(realpath ./../zmk-lily59-main/config) -DSHIELD="lily58_right nice_view_adapter nice_view" &&
-    cp build/cf-lily/right/zephyr/zmk.uf2 firmware/cf_lily_right_display.uf2
